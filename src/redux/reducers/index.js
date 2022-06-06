609b4d692861e1849fb9c40417dd9280fe067799
Andry Pebrianto <andrypeb227@gmail.com>
2022-06-06 08:08:15 +0700

    Integrasi fitur login

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import detailUserReducer from './detailUser';
+
+const rootReducers = combineReducers({
+  detailUser: detailUserReducer,
+});
+
+export default rootReducers;
---
+++ src/router/index.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import NotFound from "../pages/NotFound";
+
+function PrivateRoute({ children }) {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    return children;
+  }
+  return <Navigate to="/login" />;
+}
+
+function PublicRoute({ children }) {
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    return children;
+  }
+  return <Navigate to="/" />;
+}
+
+export default function router() {
+  return (
+    <BrowserRouter>
+      <ToastContainer />
+      <Routes>
+        <Route path="/login">
+          <Route
+            index
+            element={
+              <PublicRoute>
+                <Login />
+              </PublicRoute>
+            }
+          />
+        </Route>
+        <Route path="/register">
+          <Route
+            index
+            element={
+              <PublicRoute>
+                <Register />
+              </PublicRoute>
+            }
+          />
+        </Route>
+        <Route path="/">
+          <Route
+            index
+            element={
+              <PrivateRoute>
+                <Home />
+              </PrivateRoute>
+            }
+          />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
