f4a195f0cfff2b89338ee036ab82983ed64a8d69
Andry Pebrianto <andrypeb227@gmail.com>
2022-06-20 04:02:43 +0700

    Integrasi detail receiver

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import moment from 'moment';
+import Drawer from 'react-modern-drawer';
+import 'react-modern-drawer/dist/index.css';
 
 export default function Chat({
   activeReceiver,
@@ -11,6 +13,11 @@ export default function Chat({
   setMessage,
   onDeleteMessage,
 }) {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const toggleDrawer = () => {
+    setIsOpen((prevState) => !prevState);
+  };
+
   return (
     <>
       {activeReceiver ? (
@@ -25,18 +32,20 @@ export default function Chat({
                 <>
                   {detailReceiver.data.photo ? (
                     <img
-                      className="profile-rounded"
+                      className="profile-rounded pointer"
                       src={`https://drive.google.com/uc?export=view&id=${detailReceiver.data.photo}`}
                       alt="Gambar Profile"
+                      onClick={toggleDrawer}
                     />
                   ) : (
                     <img
-                      className="profile-rounded"
+                      className="profile-rounded pointer"
                       src="https://images227.netlify.app/mernuas/default.jpg"
                       alt="Gambar Profile"
+                      onClick={toggleDrawer}
                     />
                   )}
-                  <div className="ms-3">
+                  <div className="ms-3 pointer" onClick={toggleDrawer}>
                     <p className="fw-bold m-0 p-0">
                       {detailReceiver.data.username}
                     </p>
@@ -44,6 +53,57 @@ export default function Chat({
                       <small>Online</small>
                     </p>
                   </div>
+                  <Drawer
+                    open={isOpen}
+                    onClose={toggleDrawer}
+                    direction="right"
+                    className="bla bla bla"
+                    size={350}
+                  >
+                    <div className="p-3">
+                      <div className="profile mt-4 profile">
+                        <div className="position-relative">
+                          {detailReceiver.data.photo ? (
+                            <img
+                              className="profile-rounded"
+                              src={`https://drive.google.com/uc?export=view&id=${detailReceiver.data.photo}`}
+                              alt="Gambar Profile"
+                            />
+                          ) : (
+                            <img
+                              className="profile-rounded"
+                              src="https://images227.netlify.app/mernuas/default.jpg"
+                              alt="Gambar Profile"
+                            />
+                          )}
+                        </div>
+                        <h5 className="fw-bold mt-3">
+                          {detailReceiver.data.username}
+                        </h5>
+                        <p>{detailReceiver.data.email}</p>
+                        <div className="w-100 mt-3">
+                          {
+                            detailReceiver.data.phone && (
+                            <p>
+                              <b>Phone:</b>
+                              {' '}
+                              {detailReceiver.data.phone}
+                            </p>
+                            )
+                          }
+                          {
+                            detailReceiver.data.bio && (
+                            <p>
+                              <b>Bio:</b>
+                              {' '}
+                              {detailReceiver.data.bio}
+                            </p>
+                            )
+                          }
+                        </div>
+                      </div>
+                    </div>
+                  </Drawer>
                 </>
               )}
             </div>
@@ -63,19 +123,21 @@ export default function Chat({
                           <div>
                             <div className="d-flex justify-content-end align-items-end mt-4">
                               <div className="ballon-right me-2">
-                                {
-                                  chat.is_deleted ? <p className="p-0 m-0 text-secondary">This message has been deleted</p> : (
-                                    <>
-                                      <p className="p-0 m-0">{chat.chat}</p>
-                                      <small
-                                        className="text-secondary"
-                                        style={{ fontSize: '13px' }}
-                                      >
-                                        {moment(chat.date).format('LLL')}
-                                      </small>
-                                    </>
-                                  )
-                                }
+                                {chat.is_deleted ? (
+                                  <p className="p-0 m-0 text-secondary">
+                                    This message has been deleted
+                                  </p>
+                                ) : (
+                                  <>
+                                    <p className="p-0 m-0">{chat.chat}</p>
+                                    <small
+                                      className="text-secondary"
+                                      style={{ fontSize: '13px' }}
+                                    >
+                                      {moment(chat.date).format('LLL')}
+                                    </small>
+                                  </>
+                                )}
                               </div>
                               {chat.photo ? (
                                 <img
@@ -91,8 +153,7 @@ export default function Chat({
                                 />
                               )}
                             </div>
-                            {
-                              !chat.is_deleted && (
+                            {!chat.is_deleted && (
                               <div
                                 className="d-flex justify-content-end w-100"
                                 style={{ marginTop: '-12px' }}
@@ -100,12 +161,15 @@ export default function Chat({
                                 <span className="text-primary pointer mt-3 me-2">
                                   Edit
                                 </span>
-                                <span className="text-danger pointer mt-3" onClick={() => onDeleteMessage(chat)} style={{ marginRight: '65px' }}>
+                                <span
+                                  className="text-danger pointer mt-3"
+                                  onClick={() => onDeleteMessage(chat)}
+                                  style={{ marginRight: '65px' }}
+                                >
                                   Delete
                                 </span>
                               </div>
-                              )
-                            }
+                            )}
                           </div>
                         ) : (
                           <div className="d-flex justify-content-start align-items-end mt-4">
